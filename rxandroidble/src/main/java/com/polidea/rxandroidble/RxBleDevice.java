4c6a4bc6f47855c6d470bd7eabc4240b4d92cdb2
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2016-01-26 16:09:39 +0100

    Added Context parameter on RxBleDevice.establishConnection()

--- rxandroidble/src/main/java/com/polidea/rxandroidble/RxBleDevice.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/RxBleDevice.java
@@ -1,10 +1,13 @@
 package com.polidea.rxandroidble;
 
+import android.content.Context;
 import rx.Observable;
 
 public interface RxBleDevice {
 
     Observable<RxBleConnection.RxBleConnectionState> getConnectionState();
 
-    Observable<RxBleConnection> establishConnection();
+    Observable<RxBleConnection> establishConnection(Context context);
+
+    String getName();
 }
--- rxandroidble/src/main/java/com/polidea/rxandroidble/RxBleDeviceImpl.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/RxBleDeviceImpl.java
@@ -1,23 +1,39 @@
 package com.polidea.rxandroidble;
 
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import com.polidea.rxandroidble.internal.RxBleRadio;
 import rx.Observable;
 
 public class RxBleDeviceImpl implements RxBleDevice {
 
     private final BluetoothDevice bluetoothDevice;
 
-    public RxBleDeviceImpl(BluetoothDevice bluetoothDevice) {
+    private final RxBleRadio rxBleRadio;
+
+    public RxBleDeviceImpl(BluetoothDevice bluetoothDevice, RxBleRadio rxBleRadio) {
         this.bluetoothDevice = bluetoothDevice;
+        this.rxBleRadio = rxBleRadio;
     }
 
     public Observable<RxBleConnection.RxBleConnectionState> getConnectionState() {
         return null;
     }
 
-    public Observable<RxBleConnection> establishConnection() {
-        return Observable.create(subscriber -> {
-//            new RxBleConnectionImpl(bluetoothDevice) TODO
-        });
+    public Observable<RxBleConnection> establishConnection(Context context) {
+        final RxBleConnectionImpl rxBleConnection = new RxBleConnectionImpl(bluetoothDevice, rxBleRadio);
+        return rxBleConnection.connect(context);
+    }
+
+    @Override
+    public String getName() {
+        return bluetoothDevice.getName();
+    }
+
+    @Override
+    public String toString() {
+        return "RxBleDeviceImpl{" +
+                "bluetoothDevice=" + bluetoothDevice.getName() + '(' + bluetoothDevice.getAddress() + ')' +
+                '}';
     }
 }
