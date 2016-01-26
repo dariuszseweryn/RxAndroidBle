71424d32fb8539308883dda66eea852cc97a5dd2
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2016-01-26 15:01:50 +0100

    Added description to RxBleConnectionState (for debugging purposes)

--- rxandroidble/src/main/java/com/polidea/rxandroidble/RxBleConnection.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/RxBleConnection.java
@@ -11,13 +11,27 @@ public interface RxBleConnection {
     // TODO: 26.01.2016 [PU] Why not an enum?
     class RxBleConnectionState {
 
-        public static final RxBleConnectionState DISCONNECTING = new RxBleConnectionState();
+        public static final RxBleConnectionState DISCONNECTING = new RxBleConnectionState("DISCONNECTING");
 
-        public static final RxBleConnectionState DISCONNECTED = new RxBleConnectionState();
+        public static final RxBleConnectionState DISCONNECTED = new RxBleConnectionState("DISCONNECTED");
 
-        public static final RxBleConnectionState CONNECTING = new RxBleConnectionState();
+        public static final RxBleConnectionState CONNECTING = new RxBleConnectionState("CONNECTING");
 
-        public static final RxBleConnectionState CONNECTED = new RxBleConnectionState();
+        public static final RxBleConnectionState CONNECTED = new RxBleConnectionState("CONNECTED");
+
+        private final String description;
+
+        public RxBleConnectionState(String description) {
+
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return "RxBleConnectionState{" +
+                    description +
+                    '}';
+        }
     }
 
     Observable<RxBleConnection> connect(Context context); // TODO: hide from the user
