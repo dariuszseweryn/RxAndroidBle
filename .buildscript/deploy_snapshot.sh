c8c1fd81ee95316e21b17bff1daf6ffeac8cfbda
pawel.urban <pawel.urban@gmail.com>
2016-02-09 13:54:27 +0100

    Publish on all builds.

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -12,16 +12,16 @@ BRANCH="master"
 set -e
 echo "SLUG $TRAVIS_REPO_SLUG / JDK $TRAVIS_JDK_VERSION / PR $TRAVIS_PULL_REQUEST / BRANCH $TRAVIS_BRANCH"
 
-if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
-  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
-  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
-elif [ "$TRAVIS_PULL_REQUEST" != "true" ]; then
-  echo "Skipping snapshot deployment: was pull request."
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
-else
+#if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+#  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+#elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+#  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
+#elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#  echo "Skipping snapshot deployment: was pull request."
+#elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+#  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+#else
   echo "Deploying snapshot..."
   ./gradlew uploadArchives
   echo "Snapshot deployed!"
-fi
+#fi
