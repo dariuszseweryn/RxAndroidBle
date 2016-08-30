68308518a5bc6d99d8084b528d47e75fe70bceeb
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2016-08-30 13:11:41 +0200

    Added link to `gatt_api.h` in BleGattException javadoc for reference of the BluetoothGatt status.

--- rxandroidble/src/main/java/com/polidea/rxandroidble/exceptions/BleGattException.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/exceptions/BleGattException.java
@@ -3,7 +3,8 @@ package com.polidea.rxandroidble.exceptions;
 /**
  * Exception emitted when the BLE link has been interrupted as a result of an error. The exception contains
  * detailed explanation of the error source (type of operation) and the code proxied from
- * the Android system.
+ * the <a href="https://android.googlesource.com/platform/external/bluetooth/bluedroid/+/android-5.1.0_r1/stack/include/gatt_api.h">
+ * Android system</a>.
  *
  * @see com.polidea.rxandroidble.RxBleDevice#establishConnection(Context, boolean)
  */
