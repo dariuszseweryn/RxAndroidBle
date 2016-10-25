484e88f00eebf962be50dd39916a146d91428d21
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2016-10-25 15:44:07 +0200

    Fixed calling `BluetoothGatt.disconnect()` on a proper thread.
    Summary: https://github.com/Polidea/RxAndroidBle/issues/84
    
    Reviewers: michal.zielinski, pawel.urban
    
    Reviewed By: pawel.urban
    
    Differential Revision: https://phabricator.polidea.com/D1906

--- rxandroidble/src/main/java/com/polidea/rxandroidble/internal/operations/RxBleRadioOperationDisconnect.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/internal/operations/RxBleRadioOperationDisconnect.java
@@ -1,20 +1,17 @@
 package com.polidea.rxandroidble.internal.operations;
 
+import static rx.Observable.just;
+
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
-
 import com.polidea.rxandroidble.RxBleConnection;
 import com.polidea.rxandroidble.internal.RxBleRadioOperation;
 import com.polidea.rxandroidble.internal.connection.RxBleGattCallback;
-
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
-
 import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-
-import static rx.Observable.just;
+import rx.Scheduler;
 
 public class RxBleRadioOperationDisconnect extends RxBleRadioOperation<Void> {
 
@@ -22,12 +19,14 @@ public class RxBleRadioOperationDisconnect extends RxBleRadioOperation<Void> {
     private final RxBleGattCallback rxBleGattCallback;
     private final AtomicReference<BluetoothGatt> bluetoothGattAtomicReference;
     private final BluetoothManager bluetoothManager;
+    private final Scheduler mainThreadScheduler;
 
     public RxBleRadioOperationDisconnect(RxBleGattCallback rxBleGattCallback, AtomicReference<BluetoothGatt> bluetoothGattAtomicReference,
-                                         BluetoothManager bluetoothManager) {
+                                         BluetoothManager bluetoothManager, Scheduler mainThreadScheduler) {
         this.rxBleGattCallback = rxBleGattCallback;
         this.bluetoothGattAtomicReference = bluetoothGattAtomicReference;
         this.bluetoothManager = bluetoothManager;
+        this.mainThreadScheduler = mainThreadScheduler;
     }
 
     @Override
@@ -37,7 +36,7 @@ public class RxBleRadioOperationDisconnect extends RxBleRadioOperation<Void> {
                 .filter(bluetoothGatt -> bluetoothGatt != null)
                 .flatMap(bluetoothGatt -> isDisconnected(bluetoothGatt) ? just(bluetoothGatt) : disconnect(bluetoothGatt))
                 .doOnTerminate(() -> releaseRadio())
-                .observeOn(AndroidSchedulers.mainThread())
+                .observeOn(mainThreadScheduler)
                 .subscribe(
                         bluetoothGatt -> bluetoothGatt.close(),
                         throwable -> onError(throwable),
@@ -56,14 +55,26 @@ public class RxBleRadioOperationDisconnect extends RxBleRadioOperation<Void> {
      * 2. The same BluetoothGatt - in this situation we should probably cancel the pending BluetoothGatt.close() call
      */
     private Observable<BluetoothGatt> disconnect(BluetoothGatt bluetoothGatt) {
-        return rxBleGattCallback
-                .getOnConnectionStateChange()
-                .doOnSubscribe(bluetoothGatt::disconnect)
-                // It should never happen because if connection was never acquired then it will complete earlier.
-                // Just in case timeout here.
-                .timeout(TIMEOUT_DISCONNECT, TimeUnit.SECONDS, just(RxBleConnection.RxBleConnectionState.DISCONNECTED))
-                .filter(rxBleConnectionState -> rxBleConnectionState == RxBleConnection.RxBleConnectionState.DISCONNECTED)
-                .take(1)
-                .map(rxBleConnectionState -> bluetoothGatt);
+        return new DisconnectGattObservable(bluetoothGatt, rxBleGattCallback, mainThreadScheduler)
+                .timeout(TIMEOUT_DISCONNECT, TimeUnit.SECONDS, just(bluetoothGatt));
+    }
+
+    private static class DisconnectGattObservable extends Observable<BluetoothGatt> {
+
+        DisconnectGattObservable(
+                BluetoothGatt bluetoothGatt,
+                RxBleGattCallback rxBleGattCallback,
+                Scheduler disconnectScheduler
+        ) {
+            super(subscriber -> {
+                rxBleGattCallback
+                        .getOnConnectionStateChange()
+                        .filter(rxBleConnectionState -> rxBleConnectionState == RxBleConnection.RxBleConnectionState.DISCONNECTED)
+                        .take(1)
+                        .map(rxBleConnectionState -> bluetoothGatt)
+                        .subscribe(subscriber);
+                disconnectScheduler.createWorker().schedule(bluetoothGatt::disconnect);
+            });
+        }
     }
 }
