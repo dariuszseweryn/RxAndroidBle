9bca7d87056660c6a9c96f18370dc26ff1d74e6d
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2018-01-04 11:32:53 +0100

    Code review update.

--- rxandroidble/src/main/java/com/polidea/rxandroidble/exceptions/BleGattCannotStartException.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/exceptions/BleGattCannotStartException.java
@@ -4,7 +4,7 @@ import android.bluetooth.BluetoothGatt;
 
 /**
  * An exception emitted from {@link com.polidea.rxandroidble.RxBleConnection} functions when the underlying {@link BluetoothGatt}
- * returns `false` from {@link BluetoothGatt#readRemoteRssi()} or other functions.
+ * returns `false` from {@link BluetoothGatt#readRemoteRssi()} or other functions associated with device interaction.
  */
 public class BleGattCannotStartException extends BleGattException {
 
