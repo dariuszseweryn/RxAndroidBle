0ede6b77173c976086de2cb1cc69a974d158d4c5
pawel.urban <pawel.urban@gmail.com>
2018-01-16 12:38:04 +0100

    Review changes.

--- rxandroidble/src/main/java/com/polidea/rxandroidble/internal/util/OperationLogger.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/internal/util/OperationLogger.java
@@ -10,26 +10,26 @@ public class OperationLogger {
 
     public static void logOperationStarted(Operation operation) {
         if (RxBleLog.isAtLeast(RxBleLog.DEBUG)) {
-            RxBleLog.d("STARTED %s(%d)", operation.getClass().getSimpleName(), System.identityHashCode(operation));
+            RxBleLog.d("STARTED  %s(%d)", operation.getClass().getSimpleName(), System.identityHashCode(operation));
         }
     }
 
     public static void logOperationRemoved(Operation operation) {
         if (RxBleLog.isAtLeast(RxBleLog.DEBUG)) {
-            RxBleLog.d("REMOVED %s(%d)", operation.getClass().getSimpleName(), System.identityHashCode(operation));
+            RxBleLog.d("REMOVED  %s(%d)", operation.getClass().getSimpleName(), System.identityHashCode(operation));
         }
     }
 
     public static void logOperationQueued(Operation operation) {
         if (RxBleLog.isAtLeast(RxBleLog.DEBUG)) {
-            RxBleLog.d("QUEUED %s(%d)", operation.getClass().getSimpleName(), System.identityHashCode(operation));
+            RxBleLog.d("QUEUED   %s(%d)", operation.getClass().getSimpleName(), System.identityHashCode(operation));
         }
     }
 
     public static void logOperationFinished(Operation operation, long startTime, long endTime) {
         if (RxBleLog.isAtLeast(RxBleLog.DEBUG)) {
-            RxBleLog.d("FINISHED in %dms %s(%d)", (endTime - startTime), operation.getClass().getSimpleName(),
-                    System.identityHashCode(operation));
+            RxBleLog.d("FINISHED %s(%d) in %d ms", operation.getClass().getSimpleName(),
+                    System.identityHashCode(operation), (endTime - startTime));
         }
     }
 }
