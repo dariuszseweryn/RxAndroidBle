965e79fb79e40dc3df62ed1bb8f909c44a102bad
pawel.urban <pawel.urban@gmail.com>
2018-03-15 14:57:28 +0100

    [#383] Trying to fix undeliverable exception when disposing MTU watcher.

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/connection/MtuWatcher.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/connection/MtuWatcher.java
@@ -1,26 +1,33 @@
 package com.polidea.rxandroidble2.internal.connection;
 
 
+import com.polidea.rxandroidble2.exceptions.BleDisconnectedException;
+import com.polidea.rxandroidble2.internal.RxBleLog;
+
 import bleshadow.javax.inject.Inject;
 import bleshadow.javax.inject.Named;
-
 import io.reactivex.Observable;
-import io.reactivex.disposables.SerialDisposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
 
 @ConnectionScope
 class MtuWatcher implements ConnectionSubscriptionWatcher, MtuProvider, Consumer<Integer> {
 
     private Integer currentMtu;
     private final Observable<Integer> mtuObservable;
-    private final SerialDisposable serialSubscription = new SerialDisposable();
 
     @Inject
     MtuWatcher(
             final RxBleGattCallback rxBleGattCallback,
             @Named(ConnectionComponent.NamedInts.GATT_MTU_MINIMUM) final int initialValue
     ) {
-        this.mtuObservable = rxBleGattCallback.getOnMtuChanged().retry();
+        this.mtuObservable = rxBleGattCallback.getOnMtuChanged()
+                .retry(new Predicate<Throwable>() {
+                    @Override
+                    public boolean test(Throwable throwable) throws Exception {
+                        return !(throwable instanceof BleDisconnectedException);
+                    }
+                });
         this.currentMtu = initialValue;
     }
 
@@ -31,12 +38,17 @@ class MtuWatcher implements ConnectionSubscriptionWatcher, MtuProvider, Consumer
 
     @Override
     public void onConnectionSubscribed() {
-        serialSubscription.set(mtuObservable.subscribe(this));
+        mtuObservable.subscribe(this, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                RxBleLog.d(throwable, "An error received when listening to MTU changes.");
+            }
+        });
     }
 
     @Override
     public void onConnectionUnsubscribed() {
-        serialSubscription.dispose();
+        // Not required
     }
 
     @Override
