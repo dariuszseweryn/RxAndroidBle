eeb4664e7821c08312d05ed70fe5ce677b5418d1
David Medenjak <davidmedenjak@gmail.com>
2018-05-24 10:43:23 +0200

    Fixed FIFORunnableEntry stalling Queues (#428)
    Fix deadlock reported by #426 when FIFORunnableEntry emitter was disposed before the entry is run.

---
+++ rxandroidble/src/test/groovy/com/polidea/rxandroidble2/internal/serialization/FIFORunnableEntryTest.groovy
@@ -0,0 +1,91 @@
+package com.polidea.rxandroidble2.internal.serialization
+
+import com.polidea.rxandroidble2.internal.operations.Operation
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.internal.schedulers.TrampolineScheduler
+import spock.lang.Specification
+
+class FIFORunnableEntryTest extends Specification {
+
+    def mockOperation = Mock Operation
+    def mockQueueReleaseInterface = Mock QueueReleaseInterface
+    def mockObservableEmitter = Mock ObservableEmitter
+    def mockQueueSemaphore = Mock QueueSemaphore
+    def testException = new RuntimeException("test")
+
+    FIFORunnableEntry objectUnderTest
+
+    void setup() {
+        objectUnderTest = new FIFORunnableEntry(mockOperation, mockObservableEmitter)
+    }
+
+    def "should release the semaphore if ObservableEmitter.isDisposed() returns true at the time of run"() {
+        given:
+        mockObservableEmitter.isDisposed() >> true
+
+        when:
+        objectUnderTest.run(mockQueueSemaphore, TrampolineScheduler.instance())
+
+        then:
+        1 * mockQueueSemaphore.release()
+    }
+
+    def "should not run operation if ObservableEmitter.isDisposed() returns true at the time of run"() {
+        given:
+        mockObservableEmitter.isDisposed() >> true
+
+        when:
+        objectUnderTest.run(mockQueueSemaphore, TrampolineScheduler.instance())
+
+        then:
+        0 * mockOperation.run(_)
+    }
+
+    def "should run operation if ObservableEmitter.isDisposed() returns false"() {
+        given:
+        mockObservableEmitter.isDisposed() >> false
+
+        when:
+        objectUnderTest.run(mockQueueSemaphore, TrampolineScheduler.instance())
+
+        then:
+        1 * mockOperation.run(mockQueueSemaphore) >> Observable.empty()
+    }
+
+    def "should pass operation next to ObservableEmitter"() {
+        given:
+        mockObservableEmitter.isDisposed() >> false
+        mockOperation.run(_) >> Observable.just(1)
+
+        when:
+        objectUnderTest.run(mockQueueSemaphore, TrampolineScheduler.instance())
+
+        then:
+        1 * mockObservableEmitter.onNext(1)
+    }
+
+    def "should pass operation completion to ObservableEmitter"() {
+        given:
+        mockObservableEmitter.isDisposed() >> false
+        mockOperation.run(_) >> Observable.empty()
+
+        when:
+        objectUnderTest.run(mockQueueSemaphore, TrampolineScheduler.instance())
+
+        then:
+        1 * mockObservableEmitter.onComplete()
+    }
+
+    def "should pass operation error to ObservableEmitter"() {
+        given:
+        mockObservableEmitter.isDisposed() >> false
+        mockOperation.run(_) >> Observable.error(testException)
+
+        when:
+        objectUnderTest.run(mockQueueSemaphore, TrampolineScheduler.instance())
+
+        then:
+        1 * mockObservableEmitter.tryOnError(testException)
+    }
+}
