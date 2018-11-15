85846e5e5c7eb028383cba52922b3812c389d5ac
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2018-11-15 01:14:31 +0100

    fix build errors in HexString

--- sample/src/main/java/com/polidea/rxandroidble2/sample/util/HexString.kt
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/util/HexString.kt
@@ -8,8 +8,8 @@ object HexString {
         val hexChars = CharArray(bytes.size * 2)
 
         for (j in bytes.indices) {
-            val v = bytes[j] and 0xFF
-            hexChars[j * 2] = HEX_ARRAY[v.ushr(4)]
+            val v = bytes[j].toInt() and 0xFF
+            hexChars[j * 2] = HEX_ARRAY[v ushr 4]
             hexChars[j * 2 + 1] = HEX_ARRAY[v and 0x0F]
         }
 
@@ -35,4 +35,4 @@ object HexString {
 
         return data
     }
-}// Utility class.
+}
