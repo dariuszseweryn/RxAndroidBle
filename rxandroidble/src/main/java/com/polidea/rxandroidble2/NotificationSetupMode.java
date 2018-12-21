df0d193043c29eb063217f88485939c4286f51b3
Sebastian Roth <sebastian.roth@gmail.com>
2018-12-21 22:11:22 +0800

    Fixes typo in NotificationSetupMode.DEFAULT (#515)

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/NotificationSetupMode.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/NotificationSetupMode.java
@@ -2,7 +2,7 @@ package com.polidea.rxandroidble2;
 
 public enum NotificationSetupMode {
     /**
-     * Configures notifications according to the standard. First by enabling the notificaiton for a selected characteristic, then
+     * Configures notifications according to the standard. First by enabling the notification for a selected characteristic, then
      * setting up the descriptor to enable notifications.
      */
     DEFAULT,
