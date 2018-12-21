56d832737885843ce50178eb4e2d6a9f49cf4479
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2018-12-21 17:10:26 +0100

    Fixed develop after merge.

--- sample/src/main/java/com/polidea/rxandroidble2/sample/util/LocationPermission.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/util/LocationPermission.java
@@ -5,8 +5,8 @@ import android.Manifest.permission;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class LocationPermission {
 
