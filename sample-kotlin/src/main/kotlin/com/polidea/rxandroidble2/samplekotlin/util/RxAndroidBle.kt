17d98c0ecfc8402e8b9db15da9dc4dc0f3a83d66
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2019-01-20 23:43:17 +0100

    add service discovery activity

--- sample-kotlin/src/main/kotlin/com/polidea/rxandroidble2/samplekotlin/util/RxAndroidBle.kt
+++ sample-kotlin/src/main/kotlin/com/polidea/rxandroidble2/samplekotlin/util/RxAndroidBle.kt
@@ -1,7 +1,10 @@
-package com.polidea.rxandroidble2.sample.util
+package com.polidea.rxandroidble2.samplekotlin.util
 
 import com.polidea.rxandroidble2.RxBleConnection
 import com.polidea.rxandroidble2.RxBleDevice
 
+/**
+ * Returns `true` if connection state is [CONNECTED][RxBleConnection.RxBleConnectionState.CONNECTED].
+ */
 internal val RxBleDevice.isConnected: Boolean
     get() = connectionState == RxBleConnection.RxBleConnectionState.CONNECTED
