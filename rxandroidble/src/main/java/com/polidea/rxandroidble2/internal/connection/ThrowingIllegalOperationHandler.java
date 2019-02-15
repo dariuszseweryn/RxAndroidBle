3ca61bb606a6c364282a18ab320c82bc1ef44011
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2019-02-15 16:50:12 +0100

    Fixed checkstyle.

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/connection/ThrowingIllegalOperationHandler.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/connection/ThrowingIllegalOperationHandler.java
@@ -3,7 +3,6 @@ package com.polidea.rxandroidble2.internal.connection;
 import android.bluetooth.BluetoothGattCharacteristic;
 
 import com.polidea.rxandroidble2.internal.BleIllegalOperationException;
-import com.polidea.rxandroidble2.internal.RxBleLog;
 
 import bleshadow.javax.inject.Inject;
 
