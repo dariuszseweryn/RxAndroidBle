f8fa1313f2f014d7430f42815ab0fd0524cbed41
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2019-05-13 14:44:58 +0200

    Fixed unregistering a BroadcastReceiver too early in LocationServicesOkObservableApi23Factory
    Due to a race condition and subscribing/unsubscribing from different threads it may have happened that unregistering was made before registering finished.

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/util/LocationServicesOkObservableApi23Factory.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/util/LocationServicesOkObservableApi23Factory.java
@@ -9,28 +9,34 @@ import android.location.LocationManager;
 import android.os.Build;
 
 import bleshadow.javax.inject.Inject;
+import bleshadow.javax.inject.Named;
+import com.polidea.rxandroidble2.ClientComponent;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Scheduler;
 import io.reactivex.functions.Cancellable;
 
 @TargetApi(Build.VERSION_CODES.KITKAT)
 public class LocationServicesOkObservableApi23Factory {
     private final Context context;
     private final LocationServicesStatus locationServicesStatus;
+    private final Scheduler bluetoothInteractionScheduler;
 
     @Inject
     LocationServicesOkObservableApi23Factory(
             final Context context,
-            final LocationServicesStatus locationServicesStatus) {
+            final LocationServicesStatus locationServicesStatus,
+            @Named(ClientComponent.NamedSchedulers.BLUETOOTH_INTERACTION) final Scheduler bluetoothInteractionScheduler) {
         this.context = context;
         this.locationServicesStatus = locationServicesStatus;
+        this.bluetoothInteractionScheduler = bluetoothInteractionScheduler;
     }
 
     public Observable<Boolean> get() {
         return Observable.create(new ObservableOnSubscribe<Boolean>() {
             @Override
-            public void subscribe(final ObservableEmitter<Boolean> emitter) throws Exception {
+            public void subscribe(final ObservableEmitter<Boolean> emitter) {
                 final boolean initialValue = locationServicesStatus.isLocationProviderOk();
                 final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
                     @Override
@@ -43,11 +49,14 @@ public class LocationServicesOkObservableApi23Factory {
                 context.registerReceiver(broadcastReceiver, new IntentFilter(LocationManager.MODE_CHANGED_ACTION));
                 emitter.setCancellable(new Cancellable() {
                     @Override
-                    public void cancel() throws Exception {
+                    public void cancel() {
                         context.unregisterReceiver(broadcastReceiver);
                     }
                 });
             }
-        }).distinctUntilChanged();
+        })
+                .distinctUntilChanged()
+                .subscribeOn(bluetoothInteractionScheduler)
+                .unsubscribeOn(bluetoothInteractionScheduler);
     }
 }
