bc4f1666c89719e64f44a5dad6e4c16c3abbef63
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2019-09-18 19:23:23 +0200

    A draft implementation

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/scan/ScanSetupBuilderImplApi23.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/scan/ScanSetupBuilderImplApi23.java
@@ -1,10 +1,14 @@
 package com.polidea.rxandroidble2.internal.scan;
 
 
+import static com.polidea.rxandroidble2.internal.scan.ExternalScanSettingsExtension.API2_EMULATED_FILTERS;
+import static com.polidea.rxandroidble2.internal.scan.ExternalScanSettingsExtension.API2_EMULATED_SETTINGS;
+
 import androidx.annotation.RequiresApi;
 import androidx.annotation.RestrictTo;
 
 import com.polidea.rxandroidble2.internal.operations.ScanOperationApi21;
+import com.polidea.rxandroidble2.internal.util.ObservableUtil;
 import com.polidea.rxandroidble2.internal.util.RxBleAdapterWrapper;
 import com.polidea.rxandroidble2.scan.ScanFilter;
 import com.polidea.rxandroidble2.scan.ScanSettings;
@@ -21,16 +25,19 @@ public class ScanSetupBuilderImplApi23 implements ScanSetupBuilder {
 
     private final RxBleAdapterWrapper rxBleAdapterWrapper;
     private final InternalScanResultCreator internalScanResultCreator;
+    private final ScanSettingsEmulator scanSettingsEmulator;
     private final AndroidScanObjectsConverter androidScanObjectsConverter;
 
     @Inject
     ScanSetupBuilderImplApi23(
             RxBleAdapterWrapper rxBleAdapterWrapper,
             InternalScanResultCreator internalScanResultCreator,
+            ScanSettingsEmulator scanSettingsEmulator,
             AndroidScanObjectsConverter androidScanObjectsConverter
     ) {
         this.rxBleAdapterWrapper = rxBleAdapterWrapper;
         this.internalScanResultCreator = internalScanResultCreator;
+        this.scanSettingsEmulator = scanSettingsEmulator;
         this.androidScanObjectsConverter = androidScanObjectsConverter;
     }
 
@@ -38,6 +45,7 @@ public class ScanSetupBuilderImplApi23 implements ScanSetupBuilder {
     @Override
     public List<ScanSetup> build(ScanSettings scanSettings, ScanFilter... scanFilters) {
         // for now assuming that on Android 6.0+ there are no problems
+        scanFilters[0].isAllFieldsEmpty();
 
         if (scanSettings.getCallbackType() != ScanSettings.CALLBACK_TYPE_ALL_MATCHES && scanFilters.length == 0) {
             // native matching does not work with no filters specified - see https://issuetracker.google.com/issues/37127640
@@ -46,21 +54,37 @@ public class ScanSetupBuilderImplApi23 implements ScanSetupBuilder {
             };
         }
         ArrayList<ScanSetup> scanSetups = new ArrayList<>();
-        scanSetups.add(new ScanSetup(
-                new ScanOperationApi21(
-                        rxBleAdapterWrapper,
-                        internalScanResultCreator,
-                        androidScanObjectsConverter,
-                        scanSettings,
-                        new EmulatedScanFilterMatcher(),
-                        scanFilters),
-                new ObservableTransformer<RxBleInternalScanResult, RxBleInternalScanResult>() {
-                    @Override
-                    public Observable<RxBleInternalScanResult> apply(Observable<RxBleInternalScanResult> observable) {
-                        return observable;
-                    }
-                }
-        ));
+        for (int emulationFlag : scanSettings.getEmulationFlags()) {
+            final boolean emulateSettings = (emulationFlag & API2_EMULATED_SETTINGS) != 0;
+            final ScanSettings resultScanSettings = emulateSettings
+                    ? new ScanSettings.Builder()
+                    .setScanMode(scanSettings.getScanMode())
+                    .setShouldCheckLocationServicesState(scanSettings.shouldCheckLocationProviderState())
+                    .build()
+                    : scanSettings;
+            ObservableTransformer<RxBleInternalScanResult, RxBleInternalScanResult> resultTransformer = emulateSettings
+                    ? scanSettingsEmulator.emulateCallbackType(scanSettings.getCallbackType())
+                    : ObservableUtil.<RxBleInternalScanResult>identityTransformer();
+
+            boolean emulateFilters = (emulationFlag & API2_EMULATED_FILTERS) != 0;
+            EmulatedScanFilterMatcher resultEmulatedScanFilterMatcher = emulateFilters
+                    ? new EmulatedScanFilterMatcher(scanFilters)
+                    : new EmulatedScanFilterMatcher();
+            ScanFilter[] resultScanFilters = emulateFilters
+                    ? null
+                    : scanFilters;
+
+            scanSetups.add(new ScanSetup(
+                    new ScanOperationApi21(
+                            rxBleAdapterWrapper,
+                            internalScanResultCreator,
+                            androidScanObjectsConverter,
+                            resultScanSettings,
+                            resultEmulatedScanFilterMatcher,
+                            resultScanFilters),
+                    resultTransformer
+            ));
+        }
         return scanSetups;
     }
 }
