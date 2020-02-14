3bdab5594baad638ef5cc2e6973aae29c356d7fe
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2020-02-14 17:04:25 +0100

    Quick-fix for Android API >= 29 BLE scan permission.

--- rxandroidble/src/main/java/com/polidea/rxandroidble/internal/util/CheckerLocationPermission.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/internal/util/CheckerLocationPermission.java
@@ -4,6 +4,7 @@ package com.polidea.rxandroidble.internal.util;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Process;
 
 import bleshadow.javax.inject.Inject;
@@ -18,8 +19,13 @@ public class CheckerLocationPermission {
     }
 
     boolean isLocationPermissionGranted() {
-        return isPermissionGranted(Manifest.permission.ACCESS_COARSE_LOCATION)
-                || isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (Math.min(Build.VERSION.SDK_INT, provideTargetSdk()) < 29) {
+            return isPermissionGranted(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    || isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        } else {
+            return isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
     }
 
     /**
@@ -33,5 +39,13 @@ public class CheckerLocationPermission {
         }
 
         return context.checkPermission(permission, android.os.Process.myPid(), Process.myUid()) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private int provideTargetSdk() {
+        try {
+            return context.getPackageManager().getApplicationInfo(context.getPackageName(), 0).targetSdkVersion;
+        } catch (Throwable catchThemAll) {
+            return Integer.MAX_VALUE;
+        }
     }
 }
