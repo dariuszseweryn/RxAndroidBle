1552f8439dc5eea7da86c51d9291e48c26f11628
Nick Brook <nrbrook@gmail.com>
2020-07-27 11:43:06 +0100

    Add documentation to ScanResultInterface

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/scan/ScanResultInterface.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/scan/ScanResultInterface.java
@@ -1,13 +1,28 @@
 package com.polidea.rxandroidble2.scan;
 
 public interface ScanResultInterface {
+    /**
+     * Get the address from the device
+     */
     String getAddress();
 
+    /**
+     * Get the RSSI of the scan result
+     */
     int getRssi();
 
+    /**
+     * Get the scan record
+     */
     ScanRecord getScanRecord();
 
+    /**
+     * Get the timestamp the scan result was produced
+     */
     long getTimestampNanos();
 
+    /**
+     * Get the type of scan callback
+     */
     ScanCallbackType getScanCallbackType();
 }
