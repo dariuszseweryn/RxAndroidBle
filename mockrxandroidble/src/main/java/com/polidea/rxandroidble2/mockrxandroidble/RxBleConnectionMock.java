58b9794efe198ed5455390cdb0dd5142e4ee5473
Nick Brook <nrbrook@gmail.com>
2020-08-04 16:15:05 +0100

    Fix uses of deprecated APIs

--- mockrxandroidble/src/main/java/com/polidea/rxandroidble2/mockrxandroidble/RxBleConnectionMock.java
+++ mockrxandroidble/src/main/java/com/polidea/rxandroidble2/mockrxandroidble/RxBleConnectionMock.java
@@ -412,7 +412,7 @@ public class RxBleConnectionMock implements RxBleConnection {
                         bluetoothGattDescriptor.setValue(data);
                     }
                 })
-                .toCompletable();
+                .ignoreElement();
     }
 
     @Override
