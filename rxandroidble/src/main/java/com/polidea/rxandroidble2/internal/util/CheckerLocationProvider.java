1af02da99d612376b5293302e0507e9588139d1a
Dariusz Seweryn <dariusz.seweryn@gmail.com>
2021-05-03 12:37:45 +0200

    Fixed Gradle deprecation warning in CheckerLocationProvider.
    Android Studio does honor a comment `//noinspection deprecation` but Gradle continues to complain. To silence Gradle we need to use `@SuppressWarnings`. We want to suppress on the smallest possible scope to not be blind about next deprecations. After refactoring to a separate function and adding `@RequiresApi()` Android Studio does not report deprecations but Gradle still complains. To fix Gradle we need to add `@SuppressWarnings("deprecation")` but the Android Studio complains about redundant deprecation. To silence that we need to add "RedundantSuppression". Looks like a bug.

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/util/CheckerLocationProvider.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/util/CheckerLocationProvider.java
@@ -1,16 +1,18 @@
 package com.polidea.rxandroidble2.internal.util;
 
 
-import android.annotation.TargetApi;
 import android.content.ContentResolver;
 import android.location.LocationManager;
 
 import android.os.Build;
 import android.provider.Settings;
+
+import androidx.annotation.RequiresApi;
+
 import bleshadow.javax.inject.Inject;
+
 import com.polidea.rxandroidble2.internal.RxBleLog;
 
-@TargetApi(19 /* Build.VERSION_CODES.KITKAT */)
 public class CheckerLocationProvider {
 
     private final ContentResolver contentResolver;
@@ -27,13 +29,26 @@ public class CheckerLocationProvider {
             return locationManager.isLocationEnabled();
         }
         if (Build.VERSION.SDK_INT >= 19 /* Build.VERSION_CODES.KITKAT */) {
-            try {
-                //noinspection deprecation
-                return Settings.Secure.getInt(contentResolver, Settings.Secure.LOCATION_MODE) != Settings.Secure.LOCATION_MODE_OFF;
-            } catch (Settings.SettingNotFoundException e) {
-                RxBleLog.w(e, "Could not use LOCATION_MODE check. Falling back to a legacy/heuristic function.");
-            }
+            return isLocationProviderEnabledBelowApi28();
         }
+        return isLocationProviderEnabledBelowApi19();
+    }
+
+    // Android Studio does not see the deprecation of Settings.Secure.LOCATION_MODE in this method (if you the same code it to
+    // `isLocationProviderEnabled` then AS will properly see it). On the other hand Gradle will complain that a deprecated code is used.
+    // To silence Gradle warning a @SuppressWarnings is needed. @SuppressWarnings triggers a warning in AS as a redundant suppression.
+    @SuppressWarnings({"deprecation", "RedundantSuppression"})
+    @RequiresApi(19 /* Build.VERSION_CODES.KITKAT */)
+    private boolean isLocationProviderEnabledBelowApi28() {
+        try {
+            return Settings.Secure.getInt(contentResolver, Settings.Secure.LOCATION_MODE) != Settings.Secure.LOCATION_MODE_OFF;
+        } catch (Settings.SettingNotFoundException e) {
+            RxBleLog.w(e, "Could not use LOCATION_MODE check. Falling back to a legacy/heuristic function.");
+            return isLocationProviderEnabledBelowApi19();
+        }
+    }
+
+    private boolean isLocationProviderEnabledBelowApi19() {
         return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
