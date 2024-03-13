f4bf0c3cf027a8c463fb9596972ad01b8c931c75
Dariusz Seweryn <dariusz.seweryn@gmail.com>
2024-03-13 22:08:50 +0100

    Migrate BleScanExceptionTest. (failure expected)

---
+++ rxandroidble/src/test/java/com/polidea/rxandroidble2/exceptions/BleScanExceptionTest.java
@@ -0,0 +1,17 @@
+package com.polidea.rxandroidble2.exceptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BleScanExceptionTest {
+
+    @Test
+    public void toStringShouldContainMessage() {
+        // given
+        BleScanException out = new BleScanException(BleScanException.BLUETOOTH_DISABLED);
+
+        // expect
+        assertEquals(out.toString(), "sscom.polidea.rxandroidble2.exceptions.BleScanException: Bluetooth disabled (code 1)");
+    }
+}
